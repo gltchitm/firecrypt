20614ef9f12d9132d1f065f9a8a99885be34d113
gltchitm <42951031+gltchitm@users.noreply.github.com>
2022-06-12 15:55:55 -0400

    fix icon generation

--- scripts/darwin/release.sh
+++ scripts/darwin/release.sh
@@ -19,17 +19,17 @@ if ! [ -f icon/darwin/icon.icns ]; then
     cd Firecrypt.iconset
 
     sips -z 16 16 ../icon.png --out icon_16x16.png
-    sips -z 32 32 ../icon.png --out icon_16x162x.png
+    sips -z 32 32 ../icon.png --out icon_16x16@2x.png
     sips -z 32 32 ../icon.png --out icon_32x32.png
-    sips -z 64 64 ../icon.png --out icon_32x322x.png
+    sips -z 64 64 ../icon.png --out icon_32x32@2x.png
     sips -z 64 64 ../icon.png --out icon_64x64.png
-    sips -z 128 128 ../icon.png --out icon_64x642x.png
+    sips -z 128 128 ../icon.png --out icon_64x64@2x.png
     sips -z 128 128 ../icon.png --out icon_128x128.png
-    sips -z 256 256 ../icon.png --out icon_128x1282x.png
+    sips -z 256 256 ../icon.png --out icon_128x128@2x.png
     sips -z 256 256 ../icon.png --out icon_256x256.png
-    sips -z 512 512 ../icon.png --out icon_256x2562x.png
+    sips -z 512 512 ../icon.png --out icon_256x256@2x.png
     sips -z 512 512 ../icon.png --out icon_512x512.png
-    sips -z 1024 1024 ../icon.png --out icon_512x5122x.png
+    sips -z 1024 1024 ../icon.png --out icon_512x512@2x.png
     sips -z 1024 1024 ../icon.png --out icon_1024.png
 
     cd ..
@@ -44,7 +44,6 @@ fi
 mkdir -p release/darwin/Firecrypt.app/Contents
 mkdir release/darwin/Firecrypt.app/Contents/MacOS
 mkdir release/darwin/Firecrypt.app/Contents/Resources
-mkdir release/darwin/Firecrypt.app/Contents/Resources/icon
 
 set +e
 version=$(git describe --tags --abbrev=0 2>/dev/null)
@@ -68,7 +67,7 @@ cat << EOF > release/darwin/Firecrypt.app/Contents/Info.plist
     <key>CFBundleExecutable</key>
     <string>firecrypt</string>
     <key>CFBundleIconFile</key>
-    <string>icon/icon.icns</string>
+    <string>icon.icns</string>
     <key>CFBundleIdentifier</key>
     <string>com.github.gltchitm.Firecrypt</string>
     <key>CFBundlePackageType</key>
@@ -81,9 +80,8 @@ cat << EOF > release/darwin/Firecrypt.app/Contents/Info.plist
 </plist>
 EOF
 
-cp NOTICE release/darwin/Firecrypt.app/Contents/Resources/NOTICE
-cp icon/darwin/icon.icns release/darwin/Firecrypt.app/Contents/Resources/icon/icon.icns
-cp icon/NOTICE release/darwin/Firecrypt.app/Contents/Resources/icon/NOTICE
+cp icon/darwin/icon.icns release/darwin/Firecrypt.app/Contents/Resources/icon.icns
+
 cp -r ui/* release/darwin/Firecrypt.app/Contents/Resources
 
 go clean --cache
